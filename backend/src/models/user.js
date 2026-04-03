321d703dc44735c583e132d07cffa515747d1720
dhruv <dhruvsaini230@gmail.com>
2026-04-03 13:57:20 +0530

    feat(user): add User model and migration - create User model with name, email, password_hash, role_id, status, and is_deleted fields - establish foreign key relation with Role via role_id - add initial migration for Users table - prepare structure for authentication and RBAC

---
+++ backend/src/models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password_hash: DataTypes.TEXT,
+    role_id: DataTypes.INTEGER,
+    status: DataTypes.STRING,
+    is_deleted: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
